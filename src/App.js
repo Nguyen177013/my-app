2b05a75b62780c77b14ac85f46089a1a2289ed83
Copy <phannguyendoremon@gmail.com>
2023-02-19 17:45:56 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.toggleGoOut = this.toggleGoOut.bind(this);
+    this.state = {
+      goOut:"Yes"
+    }
+  }
+  toggleGoOut(){
+      this.setState(preState=>{
+        return {
+          goOut: preState.goOut === "Yes"?"No":"Yes"
+        }
+      })
+    }
+    render(){
+      return(
+        <div className="state">
+          <h1>{this.props.data}</h1>
+          <h1 className="state--title">Should I go out tonight?</h1>
+          <div className="state--value" onClick={this.toggleGoOut}>
+              <h1>{this.state.goOut}</h1>
+          </div>
+         </div>
+      )
+    }
+
 }
+// export default class App extends React.Component {
+//   state = {
+//     count:0
+//   }
+//   subtract=()=>{
+//     this.setState(preState =>{
+//         return {count:preState.count-1}
+//     });
+//   }
+//   add =() =>{
+//     this.setState(preState =>{
+//       return {count:preState.count+1}
+//   });
+//   }
+//   render(){
+//     console.log('render');
+//     return (
+//       <div className="counter">
+//           <button className="counter--minus" onClick={this.subtract}>–</button>
+//           <div className="counter--count">
+//               <h1>{this.state.count}</h1>
+//           </div>
+//           <button className="counter--plus" onClick={this.add}>+</button>
+//       </div>
+//   )
+//   }
 
-export default App;
+// }
+// export default function App() {
+//     const [goOut, setGoOut] = React.useState("Yes")
+    
+//     function toggleGoOut() {
+//         setGoOut(prevState => {
+//             return prevState === "Yes" ? "No" : "Yes"
+//         })
+//     }
+    
+//     return (
+//         <div className="state">
+//             <h1 className="state--title">Should I go out tonight?</h1>
+//             <div className="state--value" onClick={toggleGoOut}>
+//                 <h1>{goOut}</h1>
+//             </div>
+//         </div>
+//     )
+// }
